fa759a8f541f1e1ed6090c1026272b09f075e39d
Vijayalakshmi2021 <sviji1792@gmail.com>
2024-08-15 14:15:18 +0530

    Shopping Cart Task

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Footer() {
+  return  <footer class="py-5 bg-dark">
+  <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+</footer>
+}
+
+export default Footer
